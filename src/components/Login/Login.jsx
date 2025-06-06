0a32bd1ec1ab47ac520076d9cf8190c686c8d0fd
VVVu1511 <161138657+VVVu1511@users.noreply.github.com>
2025-06-06 22:27:41 +0700

    demo Login

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,54 @@
+
+function Login(){
+    return (
+        <>
+            <div>
+                <img></img>
+                <p>Đăng nhập</p>
+                <p>Bạn cần giúp đỡ</p>
+            </div>
+
+            <div>
+                <div>
+                    <div>
+                        <p>Đăng nhập</p>
+                        <div>Đăng nhập với mã QR</div>
+                        <img></img>
+                    </div>
+
+                    <input placeholder="Email/Số điện thoại/Tên đăng nhập"></input>
+                    <input placeholder="Mật khẩu">
+                        <i></i>
+                    </input>
+
+                    <button>Đăng nhập</button>
+                    <div>
+                        <p>Quên mật khẩu</p>
+                        <p>Đăng nhập với SMS</p>
+                    </div>
+
+                    <div>
+                        <span></span>
+                        <p>HOẶC</p>
+                        <span></span>
+                    </div>
+
+                    <div>
+                        <div>
+                            <img></img>
+                            <p>Facebook</p>
+                        </div>
+                        <div>
+                            <img></img>
+                            <p>Google</p>
+                        </div>
+                    </div>
+
+                    <p>Bạn mới biết đến Shopee? Đăng ký</p>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Login
